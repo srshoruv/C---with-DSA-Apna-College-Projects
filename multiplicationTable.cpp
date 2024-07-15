1a50143e8da65c6cf6bd9dc8ee69474b0eae06e4
srshoruv <srshoruv2002@gmail.com>
2024-07-15 12:27:47 +0600

    multiplication table app

---
+++ multiplicationTable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p;
+    int ans;
+    cout << "Enter the number to multiplication table: ";
+    cin >> p;
+    for (int i = 1; i <=10; i++) {
+        ans = p * i;
+        cout << p << " x " << i << " = " << ans << endl;
+    }
+}
